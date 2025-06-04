1e8122624719bbe14b34a52659dd66f309eec454
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2025-06-04 15:59:44 +0800

    Add files via upload

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,20 @@
-import discord
+import discord 
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True  # to read the messages on the server they're in
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello")
+# Bot token so that our bot connects sa discord and go online
+
+#token
+bot.run("TOKEN")
+
